5bb3806774dd1f24ebb8f9a62ba62a43a11fb6eb
Shawn Neal <sneal@sneal.net>
2022-09-09 10:30:39 -0700

    Add bosh-client-secret flag as optional

--- command/migrate.go
+++ command/migrate.go
@@ -14,9 +14,9 @@ import (
 )
 
 type Migrate struct {
-	BoshClientSecret      string `long:"bosh-client-secret" env:"BOSH_CLIENT_SECRET" description:"BoshClient client secret" required:"true"`
 	VCenterSourcePassword string `long:"source-password" env:"SOURCE_PASSWORD"  description:"password for source vcenter" required:"true"`
-	VCenterTargetPassword string `long:"target-password" env:"TARGET_PASSWORD"  description:"password for target vcenter" required:"false"`
+	VCenterTargetPassword string `long:"target-password" env:"TARGET_PASSWORD"  description:"password for target vcenter"`
+	BoshClientSecret      string `long:"bosh-client-secret" env:"BOSH_CLIENT_SECRET" description:"BoshClient client secret"`
 	ConfigFilePath        string `long:"config"  description:"path to the migrate.yml, defaults to ./migrate.yml"`
 	DryRun                bool   `long:"dry-run"  description:"does not perform any migration operations when true"`
 	Debug                 bool   `long:"debug"  description:"sets log level to debug"`
